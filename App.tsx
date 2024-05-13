21cbd8a74cc8d40d0f15afc17fee679d6b0494b0
junmo yang <jmyang-lion@github.com>
2024-05-13 20:01:56 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,101 @@
+import {Canvas} from '@react-three/fiber/native';
+import React, {Suspense} from 'react';
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import useControls from 'r3f-native-orbitcontrols';
+import {Chair} from './src/components/Chair';
+import {Gun} from './src/components/Gun';
+
+function App(): JSX.Element {
+  const [OrbitControls, events] = useControls();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.modelContainer} {...events}>
+        <Canvas>
+          <OrbitControls enablePan={false} />
+          <directionalLight position={[1, 0, 0]} args={['black', 5]} />
+          <directionalLight position={[-1, 0, 0]} args={['black', 5]} />
+          <directionalLight position={[0, 0, 1]} args={['black', 5]} />
+          <directionalLight position={[0, 0, -1]} args={['black', 5]} />
+          <directionalLight position={[0, 1, 0]} args={['black', 5]} />
+          <directionalLight position={[0, -1, 0]} args={['black', 5]} />
+          <Suspense fallback={null}>
+            {/* <Chair /> */}
+            <Gun />
+          </Suspense>
+        </Canvas>
+      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textTitle}>Chair</Text>
+          <Text style={styles.textPrice}>80.00 Matic</Text>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            mattis maximus eros, eu ullamcorper ante ullamcorper a. Phasellus
+            turpis tellus, tempus at feugiat at, facilisis ac sem.
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Pressable style={styles.button} onPress={() => {}}>
+            <Text style={styles.textButton}>Buy Now</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F7FB',
+  },
+  modelContainer: {
+    flex: 2,
+  },
+  bottomContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  textContainer: {
+    margin: 20,
+    marginBottom: 0,
+  },
+  textTitle: {
+    fontSize: 28,
+    color: '#051E47',
+    fontWeight: 'bold',
+  },
+  textPrice: {
+    fontSize: 28,
+    color: '#3F6900',
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'black',
+    fontSize: 16,
+    textAlign: 'justify',
+    marginVertical: 10,
+  },
+  buttonContainer: {
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#3F6900',
+    padding: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 24,
+  },
+  textButton: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
